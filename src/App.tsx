500511e88d4fef4d49ad75712907747fc5aa1cc2
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-05-17 20:43:15 -0400

    Setup routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./scenes/home";
+import { Store } from "./scenes/store";
+import { About } from "./scenes/about";
+
 function App() {
-  return <h1>React</h1>;
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
