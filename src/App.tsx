52d349674653742b566d5b8eb9b1b5110e4678ba
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-05-17 20:52:21 -0400

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap";
 import { Home } from "./scenes/home";
 import { Store } from "./scenes/store";
 import { About } from "./scenes/about";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/context/shoppingCartContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
